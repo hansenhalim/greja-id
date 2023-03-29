2075fb82b6e5b0f51eb84cc48c9efb7cb46260d1
V Hansen Halim <fpsecond.hh@gmail.com>
2023-03-29 10:49:54 +0700

    Add more feed tags

--- database/seeders/TagSeeder.php
+++ database/seeders/TagSeeder.php
@@ -39,7 +39,49 @@ class TagSeeder extends Seeder
         Tag::findOrCreate('Devotion', TagType::FEED_TYPE->value);
 
         Tag::findOrCreate('Iman', TagType::FEED_TAGS->value);
+        Tag::findOrCreate('Harapan', TagType::FEED_TAGS->value);
         Tag::findOrCreate('Kasih', TagType::FEED_TAGS->value);
-        Tag::findOrCreate('Pengharapan', TagType::FEED_TAGS->value);
+        Tag::findOrCreate('Hikmat', TagType::FEED_TAGS->value);
+        Tag::findOrCreate('Doa', TagType::FEED_TAGS->value);
+        Tag::findOrCreate('Kemenangan', TagType::FEED_TAGS->value);
+        Tag::findOrCreate('Pertumbuhan Rohani', TagType::FEED_TAGS->value);
+        Tag::findOrCreate('Pemulihan Diri', TagType::FEED_TAGS->value);
+        Tag::findOrCreate('Sukacita', TagType::FEED_TAGS->value);
+        Tag::findOrCreate('Damai', TagType::FEED_TAGS->value);
+        Tag::findOrCreate('Kepemimpinan', TagType::FEED_TAGS->value);
+
+        Tag::findOrCreate('Rasa Syukur', TagType::FEED_TAGS->value);
+        Tag::findOrCreate('Perbuatan Baik', TagType::FEED_TAGS->value);
+        Tag::findOrCreate('Bersaksi', TagType::FEED_TAGS->value);
+        Tag::findOrCreate('Pelayanan', TagType::FEED_TAGS->value);
+        Tag::findOrCreate('Pergaulan', TagType::FEED_TAGS->value);
+        Tag::findOrCreate('Keluarga', TagType::FEED_TAGS->value);
+        Tag::findOrCreate('Pengampunan', TagType::FEED_TAGS->value);
+        Tag::findOrCreate('Rendah Hati', TagType::FEED_TAGS->value);
+        Tag::findOrCreate('Integritas', TagType::FEED_TAGS->value);
+        Tag::findOrCreate('Sabar', TagType::FEED_TAGS->value);
+        Tag::findOrCreate('Menghormati', TagType::FEED_TAGS->value);
+        Tag::findOrCreate('Ketekunan', TagType::FEED_TAGS->value);
+
+        Tag::findOrCreate('Godaan', TagType::FEED_TAGS->value);
+        Tag::findOrCreate('Penderitaan', TagType::FEED_TAGS->value);
+        Tag::findOrCreate('Kecanduan', TagType::FEED_TAGS->value);
+        Tag::findOrCreate('Kesombongan', TagType::FEED_TAGS->value);
+        Tag::findOrCreate('Ketakutan', TagType::FEED_TAGS->value);
+        Tag::findOrCreate('Depresi', TagType::FEED_TAGS->value);
+        Tag::findOrCreate('Kekhawatiran', TagType::FEED_TAGS->value);
+        Tag::findOrCreate('Keraguan', TagType::FEED_TAGS->value);
+        Tag::findOrCreate('Ketamakan', TagType::FEED_TAGS->value);
+        Tag::findOrCreate('Kemalasan', TagType::FEED_TAGS->value);
+        Tag::findOrCreate('Kemarahan', TagType::FEED_TAGS->value);
+
+        Tag::findOrCreate('Teologi', TagType::FEED_TAGS->value);
+        Tag::findOrCreate('Okultisme', TagType::FEED_TAGS->value);
+        Tag::findOrCreate('Roh Kudus', TagType::FEED_TAGS->value);
+        Tag::findOrCreate('Tujuan', TagType::FEED_TAGS->value);
+        Tag::findOrCreate('Anugerah', TagType::FEED_TAGS->value);
+        Tag::findOrCreate('Kekudusan', TagType::FEED_TAGS->value);
+        Tag::findOrCreate('Penginjilan', TagType::FEED_TAGS->value);
+        Tag::findOrCreate('Dosa', TagType::FEED_TAGS->value);
     }
 }
