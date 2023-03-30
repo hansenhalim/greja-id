920fe73aac391cbd4b76391683262ddbdaf6cee4
V Hansen Halim <fpsecond.hh@gmail.com>
2023-03-30 16:57:12 +0700

    composer require laravel/scout

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -6,11 +6,12 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Laravel\Scout\Searchable;
 use Stancl\Tenancy\Database\Concerns\BelongsToTenant;
 
 class Member extends Model
 {
-    use HasFactory, SoftDeletes, BelongsToTenant;
+    use HasFactory, SoftDeletes, BelongsToTenant, Searchable;
 
     protected $guarded = [];
 
